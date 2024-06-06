072e723d250855c052569b0da1e24ad8062c72fe
Ditto Kim <rlawhdgus34@gmail.com>
2024-06-07 00:12:24 +0900

    initial commit of my work

---
+++ custom_drivers/my_work/007_gpio_sysfs/gpio-sysfs.c
@@ -0,0 +1,26 @@
+#include<linux/module.h>
+#include<linux/fs.h>
+#include<linux/cdev.h>
+#include<linux/device.h>
+#include<linux/kdev_t.h>
+#include<linux/uaccess.h>
+#include <linux/platform_device.h>
+#include<linux/slab.h>
+#include<linux/mod_devicetable.h>
+#include<linux/of.h>
+#include<linux/of_device.h>
+
+struct gpiodev_private_data
+{
+	char label[20];
+};
+
+struct gpiodrv_private_data
+{
+	int total_devices;
+	struct class *class_gpio;
+};
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kiran Nayak");
+MODULE_DESCRIPTION("A pseudo character platform driver which handles n platform pcdevs");
